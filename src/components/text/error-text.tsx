e7c7663292e70ade218cdcded859232cd525e170
damono999 <kancan877@gmail.com>
2022-07-23 17:20:14 +0900

    chore: margin props add error text

--- src/components/text/error-text.tsx
+++ src/components/text/error-text.tsx
@@ -5,16 +5,18 @@ import { NextProps } from '~/types/common';
 
 type ErrorTextProps = {
   text?: string;
+  margin?: string;
 };
 
-const Text = styled.p`
+const Text = styled.p<{ margin?: string }>`
   color: ${(props) => props.theme.error};
   font-size: 12px;
   margin: 0;
+  ${(props) => props.margin && 'margin: ' + props.margin};
 `;
 
-const ErrorText: NextProps<ErrorTextProps> = ({ text }) => {
-  return <>{text && <Text>{text}</Text>}</>;
+const ErrorText: NextProps<ErrorTextProps> = ({ text, margin }) => {
+  return <>{text && <Text margin={margin}>{text}</Text>}</>;
 };
 
 export default React.memo<NextProps<ErrorTextProps>>(ErrorText);
